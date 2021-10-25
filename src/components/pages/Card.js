2439cbcd856da04b20349f3e9a31d8c6ce13d2c2
arunsinghsnd <arunsinghsnd@gmail.com>
2021-10-25 17:42:17 +0530

    implement the user interface

---
+++ src/components/pages/Card.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material//Button";
+import Typography from "@mui/material/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 200,
+    marginTop: 40,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const CardPreview = () => {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">View More</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CardPreview;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,53 @@
 import React from "react";
+import "./styles.css";
+import Card from "./Card";
+import { Grid } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: "40px",
+    paddingRight: "40px",
+  },
+});
 
 const Home = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>I am Home</h1>
-    </div>
+    <Grid
+      container
+      spacing={4}
+      className={classes.gridContainer}
+      justify="center"
+    >
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+    </Grid>
   );
 };
 
