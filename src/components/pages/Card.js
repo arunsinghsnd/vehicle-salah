b2879000fa22fc76d020bec33832d5f7ed760cdc
arunsinghsnd <arunsinghsnd@gmail.com>
2021-10-27 13:43:00 +0530

    stlyes added to the card

--- src/components/pages/Card.js
+++ src/components/pages/Card.js
@@ -7,11 +7,13 @@ import Button from "@mui/material//Button";
 import Typography from "@mui/material/Typography";
 import CardMedia from "@mui/material/CardMedia";
 import { Grid } from "@mui/material";
+import { CardActionArea } from "@mui/material";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 200,
     marginTop: 40,
+    borderRadius: 20,
   },
   bullet: {
     display: "inline-block",
@@ -22,7 +24,10 @@ const useStyles = makeStyles({
     fontSize: 14,
   },
   pos: {
-    marginBottom: 12,
+    marginBottom: 3,
+  },
+  contnet: {
+    backgroundColor: "#c1e3e8",
   },
 });
 
@@ -31,52 +36,50 @@ const CardPreview = props => {
 
   return (
     <Grid item xs={12} sm={6} md={4}>
-      <Card className={classes.root} variant="outlined">
-        <CardMedia
-          component="img"
-          height="300"
-          image={props.image}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
-            {props.name}
-          </Typography>
-          <Typography variant="h5" component="h2">
-            {props.color}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {props.brand}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {props.engine}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {props.power}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {props.price}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {props.rating}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {props.seating}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {props.type}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {props.wheeler_type}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">View More</Button>
-        </CardActions>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="300"
+            image={props.image}
+            alt="green iguana"
+          />
+          <CardContent className={classes.contnet}>
+            <Typography variant="h5" component="h2" gutterBottom>
+              {props.name}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.color}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.brand}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.engine}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.power}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.price}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.rating}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.seating}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.type}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {props.wheeler_type}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">View More</Button>
+          </CardActions>
+        </CardActionArea>
       </Card>
     </Grid>
   );
