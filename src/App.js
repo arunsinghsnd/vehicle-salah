d3b99476c02d4b38e1a0c7d9f3ca951963280196
arunsinghsnd <arunsinghsnd@gmail.com>
2021-11-03 22:17:47 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
 import AppWithRouterAccess from "./components/authAdmin/AppWithRouterAccess";
 import Footer from "../src/components/layout/Footer";
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Route path="/" exact={true} component={Home} />
-        <AppWithRouterAccess />
-        <Footer />
-      </div>
-    </Router>
+    <BrowserRouter>
+      {" "}
+      <Switch>
+        <div className="App">
+          <Route path="/" exact={true} component={Home} />
+
+          <AppWithRouterAccess />
+          <Footer />
+        </div>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
