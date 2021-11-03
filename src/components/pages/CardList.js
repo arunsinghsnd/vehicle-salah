5df3c3f20aeee4f135455902a088c31fadfd7f8f
arunsinghsnd <arunsinghsnd@gmail.com>
2021-11-03 22:55:39 +0530

    fixed the bugs

--- src/components/pages/CardList.js
+++ src/components/pages/CardList.js
@@ -3,7 +3,6 @@ import Card from "./Card";
 import "./styles.css";
 
 const CardList = ({ vehicels, showMoreItem, onLoadMore }) => {
-  console.log(vehicels);
   return (
     <>
       {vehicels.slice(0, showMoreItem).map((vehicle, i) => {
